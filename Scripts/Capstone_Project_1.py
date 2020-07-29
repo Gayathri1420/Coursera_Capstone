879080e2e18f1cd858f107ef20de77d2b997a61a
Gayathri1420 <gayu20nov@gmail.com>
2020-07-28 23:16:50 -0400

    creates capstone project

---
+++ Scripts/Capstone_Project_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### This Notebook is mainly used for Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np 
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
